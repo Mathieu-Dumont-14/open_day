4d4d4cd665a55eeaf6e67ad5d408ea3d2776d06d
Mathieu-Dumont-14 <mathieu.dumont@my-digital-school.org>
2024-10-16 11:47:21 +0200

    feat: nouvelle fonctionnalité

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get('/addition', (req, res) => {
   const sum = parseInt(a) + parseInt(b);
   res.send({ result: sum });
 });
+//ajout de la soustraction
+app.get('/soustraction', (req, res) => {
+  const { a, b } = req.query;
+  const sum = parseInt(a) - parseInt(b);
+  res.send({ result: sum });
+});
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
