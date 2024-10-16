81dfe83125112f0c2fcdf1b36cf6687f32935b6a
Mathieu-Dumont-14 <mathieu.dumont@my-digital-school.org>
2024-10-16 11:28:27 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/addition', (req, res) => {
+  const { a, b } = req.query;
+  const sum = parseInt(a) + parseInt(b);
+  res.send({ result: sum });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
